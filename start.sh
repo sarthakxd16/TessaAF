49ae3660fc827eff17dddfa004a405ea2c24e1a4
Sarthak (Pricisely) <99407359+sarthakxd16@users.noreply.github.com>
2022-12-27 09:45:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,25 @@
-#!/bin/bash
-cd /app
-echo "----- Now deployed web booting your repo ------ " 
-gunicorn -b :5000 --reload --access-logfile - --error-logfile - app:app
+if [ -z $UPSTREAM_REPO ]
+
+then
+
+  echo "Cloning main Repository"
+
+  git clone https://github.com/sarthakxd16/TessaAF.git /TessaAF
+
+else
+
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+
+  git clone $UPSTREAM_REPO /TessaAF
+
+fi
+
+cd /TessaAF
+
+pip3 install -U -r requirements.txt
+
+echo "Starting Bot...."
+
+python3 bot.py
+
+https://github.com/sarthakxd16/TessaAF
